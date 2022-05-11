a3abcbaa3a49adef502c489bba3bf467322691b1
HyperGDX <1050264043@qq.com>
2022-05-11 18:00:56 +0800

    5.11

--- 5.11celiang/read_data.py
+++ 5.11celiang/read_data.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from scipy import optimize
 
 # 1 3 6 9
 # 11 12 13 14
@@ -16,8 +18,29 @@ for i in range(len(raw_df)):
     cur_t, cur_idx, _, cur_r = raw_df.iloc[i]
     if cur_idx in idx_d_map:
         t.append(cur_t)
-        d.append(idx_d_map[cur_idx])
-        r.append(cur_r)
-res = [t, d, r]
-print(res)
+        d.append(int(idx_d_map[cur_idx]))
+        r.append(-1 * int(cur_r))
+d_r_nd = np.array([d, r], dtype=np.int16)
+
+# 3*3067
+
+one_d_r = d_r_nd[:, :10]
+print("111")
+print(one_d_r)
+
+
+def d2r_func(x, a, b):
+    return a + b * np.log10(x)
+
+
+def cal_a_n(d_r):
+    x0, y0 = d_r[0], d_r[1]
+    a4, b4 = optimize.curve_fit(d2r_func, x0, y0)[0]
+    return round(a4, 4), round(b4, 4)
+
+
+print(cal_a_n(one_d_r))
+print(cal_a_n(d_r_nd))
+#
+# print(cal_a_n(raw_rssi, o_ls))
 
